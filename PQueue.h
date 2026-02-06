f691b04a53aede75ece089221096d934595ac769
Rohan Kummaraguntla <69653360+RohanKummaraguntla@users.noreply.github.com>
2026-02-06 11:06:16 -0600

    Add PQueue header file for priority queue implementation

---
+++ PQueue.h
@@ -0,0 +1,43 @@
+/*
+ * LaQueue
+ * 
+ * 
+ * This is part of a series of labs for the Liberal Arts and Science Academy.
+ * The series of labs provides a mockup of an POSIX Operating System
+ * referred to as LA(SA)nix or LAnix.
+ *  
+ * (c) copyright 2018, James Shockey - all rights reserved
+ * 
+ * */
+#ifndef __PQUEUE_H
+#define __PQUEUE_H
+struct node
+{
+    int priority;
+    void* data;
+    
+    node* link;
+};
+
+class PQueue
+{
+    private:
+        node* front;
+        long flag;
+        long current;
+    public:
+        PQueue()
+        {
+            front = nullptr;
+        }
+    
+        void push(void* item, int priority);
+        void* top();
+        void pop();
+        void display();
+        long getFlag();
+        long getCur();
+        void setFlag(long flag);
+};
+
+#endif
